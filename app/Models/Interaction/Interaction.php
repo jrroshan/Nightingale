4119f975a7658f24db2b3f711c00b9fd983f059d
Roshan Lasiwa <roshanlasiwa@gmail.com>
2022-08-27 23:30:27 +0545

    first commit

---
+++ app/Models/Interaction/Interaction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Interaction;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Interaction extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/PlaylistSong/PlaylistSong.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\PlaylistSong;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PlaylistSong extends Model
+{
+    use HasFactory;
+}
